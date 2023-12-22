a2b4d30b8bba373959339f73435fe667688719b7
firas bourguiba <fbourguiba18@gmail.com>
2023-12-22 22:42:03 +0100

    dashboard

--- src/App.js
+++ src/App.js
@@ -3,15 +3,7 @@ import {
   BrowserRouter as Router,
   Route,
   Routes,
-  Outlet,
-} from "react-router-dom"; // Import the necessary components
-import Login from "./components/authentication/Login";
-import Register from "./components/authentication/Register";
-import Products from "./components/products/Products";
-import Layout from "./components/layout/Layout";
-import ShoppingCart from "./components/payment/ShoppingCart";
-import ProductDetail from "./components/products/ProductDetail";
-import Home from "./components/home/Home";
+} from "react-router-dom";
 import { AuthProvider } from "./context/authContext";
 import Dashboard from "./components/dashboard/Dashboard";
 
@@ -20,21 +12,6 @@ function App() {
     <AuthProvider>
       <Router>
         <Routes>
-          <Route
-            path="/"
-            element={
-              <Layout>
-                <Outlet />
-              </Layout>
-            }
-          >
-            <Route index element={<Home />} />
-            <Route path="products" element={<Products />} />
-            <Route path="login" element={<Login />} />
-            <Route path="register" element={<Register />} />
-            <Route path="cart" element={<ShoppingCart />} />
-            <Route path="products/:productId" element={<ProductDetail />} />
-          </Route>
           <Route path="dashboard/:token" element={<Dashboard />} />
         </Routes>
       </Router>
